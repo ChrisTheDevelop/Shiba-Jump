736bb711721cf42d0ba8f0ebacbb6ef4beae06f7
ChrisTheDevelop <72986118+ChrisTheDevelop@users.noreply.github.com>
2021-09-08 23:58:00 +0800

    Wed

--- Shiba-Jump/Assets/Scripts/PlayerScript.cs
+++ Shiba-Jump/Assets/Scripts/PlayerScript.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerScript : MonoBehaviour
 {
     float runSpeed = 0.5f;
     float jumpSpeed = 1f;
+    float jumpCharge = 0f;
 
     bool isCrouching = false;
     bool isJumping = false;
+    bool isOnGround;
+
+    Vector2 jumpPos = new Vector2(1,10);
 
     Rigidbody2D rb;
     Animator animtor;
@@ -20,13 +27,27 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            isCrouching = true;
+        if (Input.GetButton("Jump"))
+        { 
+            isCrouching = true; 
+        }
         else isCrouching = false;
     }
 
     private void FixedUpdate()
     {
-        
+        if (isCrouching) {
+            jumpCharge += Time.fixedDeltaTime;
+        }
+        else if (jumpCharge > 0 && !isCrouching)
+        {
+            Jump(jumpCharge);
+            jumpCharge = 0;
+        }
+    }
+
+    void Jump(float jumpForce)
+    {
+        rb.AddForce(jumpPos*jumpForce, ForceMode2D.Impulse);
     }
 }
